733241ee4ccff852cb297f997456a23c7d23b68c
zhangsan <abc@163.com>
2016-11-25 17:56:54 +0800

    新建KanKan

---
+++ kankan/kankan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kankan
+//
+//  Created by qianfeng on 16/11/25.
+//  Copyright © 2016年 王鑫. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
